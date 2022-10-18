36ecc44974da1fc0431c84fbc4fc7ae1970068a5
chriswolfdesign <chriswolfdesign@gmail.com>
2022-10-18 08:47:20 -0400

    Implemented peek

--- pkg/pkg_test.go
+++ pkg/pkg_test.go
@@ -30,6 +30,9 @@ var _ = Describe("Pkg", func() {
 
 			It("has size one", func() {
 				Expect(queue.Size).To(Equal(1))
+				head, err := queue.Peek()
+				Expect(err).ToNot(HaveOccurred())
+				Expect(head).To(Equal(1))
 			})
 		})
 
@@ -41,6 +44,45 @@ var _ = Describe("Pkg", func() {
 
 			It("Has size two", func() {
 				Expect(queue.Size).To(Equal(2))
+				head, err := queue.Peek()
+				Expect(err).ToNot(HaveOccurred())
+				Expect(head).To(Equal(1))
+			})
+		})
+	})
+
+	Describe("Peek tests", func() {
+		When("peeking into empty queue", func() {
+			It("returns error", func() {
+				head, err := queue.Peek()
+				Expect(err).To(HaveOccurred())
+				Expect(head).To(Equal(0))
+			})
+		})
+
+		When("peeking into queue with one element", func() {
+			BeforeEach(func() {
+				queue.Enqueue(1)
+			})
+
+			It("returns that element with no error", func() {
+				head, err := queue.Peek()
+				Expect(err).ToNot(HaveOccurred())
+				Expect(head).To(Equal(1))
+			})
+		})
+
+		When("peeking into queue with multiple elements", func() {
+			BeforeEach(func() {
+				queue.Enqueue(1)
+				queue.Enqueue(2)
+				queue.Enqueue(3)
+			})
+
+			It("returns the first element with no error", func() {
+				head, err := queue.Peek()
+				Expect(err).ToNot(HaveOccurred())
+				Expect(head).To(Equal(1))
 			})
 		})
 	})
